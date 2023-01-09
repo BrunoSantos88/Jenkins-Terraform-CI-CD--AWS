e13ac8ae23e8c949b078576f33bd2ee794cf5139
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-09 16:36:38 -0300

    repos

--- providers.tf
+++ providers.tf
@@ -1,5 +1,10 @@
 terraform {
-  required_version = "1.3.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
 # Configure the AWS Provider
