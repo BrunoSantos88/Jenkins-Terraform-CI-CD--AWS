d192efbdbcab6812b5bb16ee34c82311c5001657
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-10 18:40:08 -0300

    backendfile

--- eksnetworking/sec.tf
+++ eksnetworking/sec.tf
@@ -1,10 +1,24 @@
 resource "aws_security_group" "web-sg" {
   name = "eks-sec-grup"
-  vpc_id = "vpc-05703b43456f02cad"
+  vpc_id = aws_vpc.eks_vpc.id
   
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000-32800
+    to_port     = 30000-32800
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -16,6 +30,3 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-
-  
