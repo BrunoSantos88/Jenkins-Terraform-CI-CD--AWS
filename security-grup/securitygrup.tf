0f73f86e7335d0539dbf2cf50b5de3c9e84cce26
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-10 18:37:24 -0300

    terraformfile

--- security-grup/securitygrup.tf
+++ security-grup/securitygrup.tf
@@ -1,50 +1,20 @@
-resource "aws_security_group" "public" {
-  name = "eks-sec"
-  description = "Public internet access"
+resource "aws_security_group" "web-sg" {
+  name = "eks-sec-grup"
   vpc_id = "vpc-05703b43456f02cad"
- 
-  tags = {
-    Name        = "public-eks-sg"
-    Project     = "cloudcasts.io"
-    ManagedBy   = "terraform"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
- 
-resource "aws_security_group_rule" "public_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
- 
-  security_group_id = aws_security_group.public.id
-}
- 
-resource "aws_security_group_rule" "public_in_ssh" {
-  type              = "ingress"
-  from_port         = 30000-32800
-  to_port           = 30000-32800
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.public.id
-}
- 
-resource "aws_security_group_rule" "public_in_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.public.id
-}
- 
-resource "aws_security_group_rule" "public_in_https" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.public.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
