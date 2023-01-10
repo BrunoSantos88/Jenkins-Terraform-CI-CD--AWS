54f3c8b76c4fd96fa13480184b3c9cc96047c8ee
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-10 18:50:47 -0300

    terraformfile

--- eksnetworking/sec.tf
+++ eksnetworking/sec.tf
@@ -16,13 +16,6 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = "30000-32800"
-    to_port     = "30000-32800"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
