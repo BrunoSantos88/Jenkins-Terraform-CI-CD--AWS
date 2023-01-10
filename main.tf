16cd1e7d4e3e43a1fe153bfa853f8479c41a8ffd
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-10 19:58:57 -0300

    terraformfile

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-module "networking" {
-  source       = "./networking"
-  vpc_cidr     = "192.0.0.0/16"
-  public_cidrs = ["192.0.1.0/24", "192.0.2.0/24"]
-}
+#module "networking" {
+ # source       = "./networking"
+ # vpc_cidr     = "192.0.0.0/16"
+ # public_cidrs = ["192.0.1.0/24", "192.0.2.0/24"]
+#}
 
 module "networking" {
 source        = "./eksnetworking"
