a45f078d675e92000e4d150af7b0dba687b8cdf2
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-10 21:44:26 -0300

    terraformfile

--- ekscluster/clustereks.tf
+++ ekscluster/clustereks.tf
@@ -2,35 +2,24 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = "terraformEKScluster"
   role_arn =  "${aws_iam_role.iam-role-eks-cluster.arn}"
   version  = "1.23"
-  vpc_config {            
-   security_group_ids = ["${aws_security_group.eks-cluster.id}"]
-   subnet_ids         = ["subnet-0f93eb1ba0436eae5","subnet-04776c3a79d7f875d"] 
-    }
 
-enable_irsa = true
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  enable_irsa = true
+
   eks_managed_node_group_defaults = {
     disk_size = 50
   }
-  
-  eks_managed_node_groups = {
-    general = {
-      desired_size = 1
-      min_size     = 1
-      max_size     = 1
+ 
+  vpc_config {            
+   security_group_ids = ["${aws_security_group.eks-cluster.id}"]
 
-      labels = {
-        role = "general"
-      }
-      instance_types = ["t3.small"]
+   subnet_ids         = ["subnet-0f93eb1ba0436eae5","subnet-04776c3a79d7f875d"] 
     }
-  tags = {
-    Environment = "staging"
-  }
-
   depends_on = [
     "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy",
     "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy",
    ]
 }
-
-}
+ 
--- ekscluster/nodeeks.tf
+++ ekscluster/nodeeks.tf
@@ -8,10 +8,22 @@ resource "aws_eks_node_group" "node" {
     max_size     = 1
     min_size     = 1
   }
+
+   labels = {
+        role = "general"
+      }
+
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
  
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
-}
+
+
+  tags = {
+    Environment = "staging"
+  }
