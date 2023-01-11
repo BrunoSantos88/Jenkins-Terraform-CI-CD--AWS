eae04dfb4b9db44542446791de7a7c0751f11f12
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-10 21:41:04 -0300

    terraformfile

--- ekscluster/clustereks.tf
+++ ekscluster/clustereks.tf
@@ -2,15 +2,35 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = "terraformEKScluster"
   role_arn =  "${aws_iam_role.iam-role-eks-cluster.arn}"
   version  = "1.23"
- # Configure EKS with vpc and network settings 
   vpc_config {            
    security_group_ids = ["${aws_security_group.eks-cluster.id}"]
-# Configure subnets below
    subnet_ids         = ["subnet-0f93eb1ba0436eae5","subnet-04776c3a79d7f875d"] 
     }
+
+enable_irsa = true
+  eks_managed_node_group_defaults = {
+    disk_size = 50
+  }
+  
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 1
+
+      labels = {
+        role = "general"
+      }
+      instance_types = ["t3.small"]
+    }
+  tags = {
+    Environment = "staging"
+  }
+
   depends_on = [
     "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy",
     "aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy",
    ]
 }
- 
+
+}
