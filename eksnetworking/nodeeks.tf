b7f3261c8a215b60d04fd6d765e15c122788d250
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-13 19:40:10 -0300

    Jenkinsfile

--- eksnetworking/nodeeks.tf
+++ eksnetworking/nodeeks.tf
@@ -3,14 +3,18 @@ resource "aws_eks_node_group" "eks-pipeline" {
   node_group_name = "eks-pipeline"
   node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = [aws_subnet.eks_subnet_public_1a.id,aws_subnet.eks_subnet_public_1b.id,aws_subnet.eks_subnet_public_1c.id]
+  
+
   scaling_config {
     desired_size = 1
     max_size     = 1
     min_size     = 1
-
-    instance_types = ["t3.medium"]
-
   }
+
+  ami_type       = "AL2_x86_64"
+  instance_types = ["t3.micro"]
+  disk_size      = 20
+
 
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
